27230154cbadcf258665dc22f6e44a569be60408
matéo! <6matbub@gmail.com>
2024-04-02 16:32:05 -0700

    feat: lift streamable content to cli

--- gulpfile.js
+++ gulpfile.js
@@ -1,121 +1,285 @@
-const gulp = require('gulp');
-const sass = require('sass');
+#!/usr/bin/env node
+const { program } = require('commander');
+const gulp = require("gulp");
+const path = require("path");
 
 // gulp plugins
-const postcssPlugin = require('gulp-postcss');
-const renamePlugin = require('gulp-rename');
-const sourcemapsPlugin = require('gulp-sourcemaps');
-const sassPlugin = require('gulp-sass')(sass);
+const postcssPlugin = require("gulp-postcss");
+const renamePlugin = require("gulp-rename");
+const sourcemapsPlugin = require("gulp-sourcemaps");
 
 // postcss plugins
-const autoprefixerPlugin = require('autoprefixer');
-const cssnanoPlugin = require('cssnano');
-const stylelintPlugin = require('stylelint');
-const reporterPlugin = require('postcss-reporter');
-const cssVariablesPlugin = require('postcss-css-variables');
-const cssMixinsPlugin = require('postcss-mixins');
-const calcPlugin = require('postcss-calc');
-const nestingPlugin = require('postcss-nesting');
-const customMediaPlugin = require('postcss-custom-media');
+const autoprefixerPlugin = require("autoprefixer");
+const cssnanoPlugin = require("cssnano");
+const stylelintPlugin = require("stylelint");
+const reporterPlugin = require("postcss-reporter");
+const cssVariablesPlugin = require("postcss-css-variables");
+const cssMixinsPlugin = require("postcss-mixins");
+const calcPlugin = require("postcss-calc");
+const nestingPlugin = require("postcss-nesting");
+const customMediaPlugin = require("postcss-custom-media");
 
+/**
+ * Represents an error that occurs during JSON parsing.
+ * @constructor
+ * @extends Error
+ * @param {string} message - The error message that explains the reason for the error.
+ */
+function JsonParsingError(message) {
+	/**
+	 * The name of the error.
+	 * @type {string}
+	 */
+	this.name = "JsonParsingError";
+
+	/**
+	 * Descriptive message associated with the error.
+	 * @type {string}
+	 */
+	this.message = message;
+
+	/**
+	 * Stack trace of the error.
+	 * @type {string}
+	 */
+	this.stack = (new Error()).stack;
+}
+
+// Setting up the prototype chain to inherit properties and methods from Error
+JsonParsingError.prototype = Object.create(Error.prototype);
+
+// Ensuring the constructor is set to JsonParsingError
+JsonParsingError.prototype.constructor = JsonParsingError;
+
+function Store() {
+	this.srcDir = "src/*.css";
+	this.buildDir = "dist/";
+	this.outsideCssVariables = {
+		variables: {
+			"--primary-color": "#FFD600",
+			"--neutrals-dark": "#000000",
+			"--neutrals-greydark": "#B9B9B9",
+			"--neutrals-grey": "#D9D9D9",
+			"--neutrals-greylight": "#F1F1F1",
+			"--neutrals-light": "#FFFFFF",
+		},
+	};
+}
+
+/**
+ * Set the source directory for the CSS files
+ * @param {String} dir 
+ * @throws {Error} - if the input is not a string
+ */
+Store.prototype.setSrcDir = function (dir) {
+	if (typeof dir !== "string") {
+		throw new Error("srcDir must be a string");
+	}
+
+	this.srcDir = dir;
+}
+
+/**
+ * Set the build directory for the CSS files
+ * @param {String} dir
+ */
+Store.prototype.setBuildDir = function (dir) {
+	if (typeof dir !== "string") {
+		throw new Error("buildDir must be a string");
+	}
+
+	this.buildDir = dir;
+}
+
+/**
+ * Set the CSS variables from an outside source
+ * @param {String} cssVariables - JSON string of CSS variables
+ * @returns {{[x as String]: String}} - JSON string of CSS variables
+ * @throws {Error} - if the input is not a string
+ */
+Store.prototype.setCssVariables = function (cssVariables = "") {
+	if (!cssVariables) {
+		return;
+	}
+
+	function parseJsonString(jsonString) {
+		try {
+			return JSON.parse(jsonString);
+		} catch (e) {
+			throw new JsonParsingError(`Error parsing JSON string: ${e.message}`);
+		}
+	}
+	
+	this.outsideCssVariables = parseJsonString(cssVariables);
+
+	return this.outsideCssVariables;	
+}
+
+// Create a new instance of the Store class
+// This is used to store the configuration settings
+const store = new Store();
+
+/**
+ * @name buildStyles - npm run build
+ * @returns {NodeJS.ReadWriteStream} - a stream of CSS files
+ */
 function buildStyles() {
 	// convert CSS variables to static values for older browsers
-	const cssVariablesConfig = cssVariablesPlugin({
-		// TODO: accept JSON string as optional CLI arg
-		variables: {
-			'--primary-color': '#FFD600',
-			'--neutrals-dark': '#000000',
-			'--neutrals-greydark': '#B9B9B9',
-			'--neutrals-grey': '#D9D9D9',
-			'--neutrals-greylight': '#F1F1F1',
-			'--neutrals-light': '#FFFFFF'
-		}
-	});
+	const cssVariablesConfig = cssVariablesPlugin(store.outsideCssVariables);
 	const cssMixinsConfig = cssMixinsPlugin({});
-	const calcConfig = calcPlugin({/* handle pixel fall back for rem values */ });
+	const calcConfig = calcPlugin({
+		/* handle pixel fall back for rem values */
+	});
 	const nestingConfig = nestingPlugin({});
 	const customMediaConfig = customMediaPlugin({});
 
-	return gulp.src('src/*.css')
+	return gulp
+		.src(store.srcDir)
 		.pipe(sourcemapsPlugin.init())
-		.pipe(postcssPlugin([
-			autoprefixerPlugin,
-			cssVariablesConfig,
-			cssMixinsConfig,
-			calcConfig,
-			nestingConfig,
-			customMediaConfig
-		]))
-		.pipe(sourcemapsPlugin.write('./maps'))
-		.pipe(gulp.dest('dist'));
+		.pipe(
+			postcssPlugin([
+				autoprefixerPlugin,
+				cssVariablesConfig,
+				cssMixinsConfig,
+				calcConfig,
+				nestingConfig,
+				customMediaConfig,
+			]),
+		)
+		.pipe(sourcemapsPlugin.write("./maps"))
+		.pipe(gulp.dest(store.buildDir));
 }
 
 /**
  * @name minifyCSS - npm run minify
- * 
+ *
  * @readme
  * Minifies CSS files and generates source maps.
- * 
+ *
  * This function takes CSS files from the 'dist' directory, minifies them,
  * generates source maps for debugging, and saves the output back to the 'dist' directory.
+ * 
+ * @returns {NodeJS.ReadWriteStream} - a stream of CSS files
  */
 function minifyCSS() {
-	return gulp.src('dist/*.css')
+	return gulp
+		.src(path.join(`${store.buildDir}/*.css`))
 		.pipe(sourcemapsPlugin.init({ loadMaps: true }))
-		.pipe(postcssPlugin([ cssnanoPlugin ]))
-		.pipe(renamePlugin({ suffix: '.min' }))
-		.pipe(sourcemapsPlugin.write('/maps'))
-		.pipe(gulp.dest('dist'));
+		.pipe(postcssPlugin([cssnanoPlugin]))
+		.pipe(renamePlugin({ suffix: ".min" }))
+		.pipe(sourcemapsPlugin.write("/maps"))
+		.pipe(gulp.dest(store.buildDir));
 }
 
+
+/**
+ * @name watch - npm run watch
+ * @readme
+ * Watches for changes in the 'src' directory and rebuilds the CSS files.
+ * 
+ * @returns {NodeJS.WritableStream} - a stream of CSS files
+ */
 function watch() {
-	const watcher = gulp.watch('src/*.css', build)
-	watcher.on('change', function(fileName) {
-		console.log('Rebuilding ' + fileName)
+	const watcher = gulp.watch(store.srcDir, build);
+	watcher.on("change", (fileName) => {
+		console.log(`Rebuilding ${fileName}`);
 	});
 }
 
+/**
+ * @name lintDevCode - npm run lint-dev
+ * @readme
+ * Lints the CSS files in the 'src' directory.
+ * 
+ * @returns {NodeJS.ReadWriteStream} - a stream of CSS files
+ */
 function lintDevCode() {
 	const stylelintRules = {
-		"color-no-invalid-hex": 2,
-	}
+		"color-no-invalid-hex": true,
+	};
 
-	const stylelintConfig = stylelintPlugin({ rules: stylelintRules })
+	const stylelintConfig = stylelintPlugin({ rules: stylelintRules });
 
 	const postcssOptions = [
 		stylelintConfig,
-		reporterPlugin({ clearMessages: true })
-	]
+		reporterPlugin({ clearMessages: true }),
+	];
 
-	return gulp.src('src/*.css')
-		.pipe(postcssPlugin(postcssOptions))
+	return gulp.src(store.srcDir).pipe(postcssPlugin(postcssOptions));
 }
 
-/** @deprecated I'm not supporting this, but it should work out of the box */
-function buildSass() {
-	const sassConfig = sassPlugin({
-		outputStyle: 'compressed'
-	})
-	const sassHandler = sassConfig.on('error', sassPlugin.logError);
-	return gulp.src('src/*.scss')
-		.pipe(sassHandler)
-		.pipe(gulp.dest('src/'))
-}
-
+// TODO Remove these tasks
+// These tasks aren't really needed; they can be used in the scripts ie `npx gulp build`; 
+// but the intent is for this to be just an abstraction of the build. They're going to be removed.
 /**
  * @name build - npm run build
- * @readme a series of tasks to build the CSS files 
- * 
+ * @readme a series of tasks to build the CSS files
+ *
  * if you don't like or want a particular task, you can remove it from the series
- * 
+ *
  * @note if you want to use scss files, you can use the `buildSass` task at your own risk
  * it would need to be treated as a preprocess step before the `buildStyles` task
  */
-const build = gulp.series(/*buildSass,*/ buildStyles, lintDevCode, minifyCSS /*...*/);
+const build = gulp.series(
+	buildStyles,
+	lintDevCode,
+	minifyCSS,
+);
 
-gulp.task('build-sass', buildSass);
-gulp.task('build-styles', buildStyles);
-gulp.task('minify', minifyCSS);
-gulp.task('default', build);
-gulp.task('lint-dev', lintDevCode);
-gulp.task('watch', watch);
+// gulp.task("build-styles", buildStyles);
+gulp.task("minify", minifyCSS);
+// gulp.task("default", build);
+gulp.task("lint-dev", lintDevCode);
+gulp.task("watch", watch);
+
+function buildAction(args) {
+	// TODO validate incoming args 
+	const { src, build: buildDir, variables } = args;
+
+	// Set the source directory for the CSS files
+	store.setSrcDir(src);
+
+	// Set the build directory for the CSS files
+	store.setBuildDir(buildDir);
+
+	// Set the CSS variables from an outside source
+	store.setCssVariables(variables);
+
+	// Programmatically run the build task
+	build((err) => {
+		if (err) {
+			throw new Error("Build task failed:", err);
+		}
+
+		console.log("Build task completed successfully.");
+	});
+}
+
+program
+	.version('0.0.1')
+	.description('CLI tool for managing PostCSS plugins');
+
+program
+	.command('build')
+	.option('-s, --src <src>', 'source directory for CSS files')
+	.option('-b, --build <build>', 'build directory for CSS files')
+	.option('-v, --variables <variables>', 'CSS variables from an outside source')
+	.description('build CSS files')
+	.action(buildAction);
+
+program
+	.command('minify')
+	.option('-s, --src <src>', 'source directory for CSS files')
+	.description('minify CSS files')
+	.action(() => {
+		// Programmatically run the minify task
+		minifyCSS((err) => {
+			if (err) {					
+					throw new Error('Minify task failed:', err);
+			}
+
+			console.log('Minify task completed successfully.');	
+		});
+	});
+
+program.parse(process.argv);
--- readme.md
+++ readme.md
@@ -1,20 +1,99 @@
-# I make themes with this
+# Css 4 Life
 
-An approachable build system for creating distributable themes using postcss as a css processor. 
+An approachable build abstraction for creating distributable themes using postcss as a css processor. Just pass your postcss files, and it spits out vanilla CSS.
 
-## Setup 
+## Benefits over CSS
 
-Just clone it, then install the dev dependencies
+- future css syntax support
+- custom media queries thanks to `postcss-custom-media`
+- automated vendor prefixes thanks to `autoprefixer`
+- `@mixin` syntax support thanks to `postcss-mixins`
+- CSS minification thanks to `cssnano`
+- math support thanks to `postcss-calc`
+- style linting thanks to `style-lint`
+- source map support thanks to `gulp-sourcemaps`
+
+Coming <strike>soon</strike> also
+- [Responsive text support](https://github.com/madeleineostoja/postcss-responsive-type)
+- [Responsive images](https://github.com/reworkcss/rework-plugin-at2x)
+
+## CLI
+
+It looks something like this, i'ma document this more when its complete 
+
+- `css4life build --srcDir="" --outputDir="" --cssVariables=""`
+
+## Local Setup 
+
+It's gotta be a multi-step process to test the executable aspect of this. The local setup consists of 3 steps. They are outlined as follows:
+
+1. [Clone and install css4life dependencies](#clone-and-install-css4life-dependencies) 
+2. [Create a test project](#create-a-test-project)
+3. [Link the two local projects together]()
+
+### Clone and install css4life dependencies
+First you'd want to clone this repo
 
 ```sh
-git clone https://github.com/matthewbub/i-make-themes-with-this
-cd i-make-themes-with-this
+git clone https://github.com/matthewbub/css4life
+cd css4life
 npm install
 ```
 
-## Scripts
+### Create a test project
+Then adjacent of the `css4life` repository, you might create a `css4life-test` directory or something.
 
-These are the two primary scripts you'd need in development. Check the scripts in the [package.json](./package.json) file for all available commands, or run `npm run` from the terminal.
+After digestion, execute the following set of commands to setup a test new project
 
-- `npm run dev` - dev mode with hot loader
-- `npm run build` - compile production code
+```sh
+# outside of css4life
+
+mkdir css4life-test
+cd css4life-test
+touch package.json
+```
+
+Inside the `css4life-test/package.json` add the minimum bits 
+
+```json
+{
+  "name": "css4life-test",
+  "version": "0.0.0",  
+	"devDependencies": {
+		"css4life": "file://../css4life"
+	}
+}
+```
+
+### Link the two local projects together
+
+Now we need to link the projects together. 
+
+**Linking css4life**
+
+In the `css4life` directory; execute `npm link` 
+
+**Linking css4life-test**
+
+Then, in the `css4life-test` directory, execute `npm link css4life`
+
+---
+
+Finally go to the `css4life-test/package.json` and add a build script
+
+```diff
+{
+  "name": "css4life-test",
+  "version": "0.0.0",  
++ "scripts": {
++		"build": "css4life build --src='test.css' --build='dist'"
++ },
+	"devDependencies": {
+		"css4life": "file://../css4life"
+	}
+}
+```
+
+Now we're able to write and compile CSS in the `css4life-test` package, and implement dev changes to the `css4life` build package without having to do anything other than save our changes and run the npm command again. 
+
+[![Travis Scott - MODERN JAM](https://img.youtube.com/vi/g8IvO7OwdaM/0.jpg)](https://www.youtube.com/watch?v=g8IvO7OwdaM)
